d1ee34d1234a923350b055ad484e5935bf5bfd47
Akansha <akansha goyal>
2019-09-27 20:28:56 +0530

    image uploaded using gallery

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 import 'dart:math' as math;
 
@@ -21,15 +24,28 @@ class _HomePageState extends State<HomePage> {
   int _imageHeight = 0;
   int _imageWidth = 0;
   String _model = "";
+  Future<File> imageFile;
 
   @override
   void initState() {
     super.initState();
   }
 
+  pickImageFromGallery(ImageSource source) {
+    setState(() {
+      imageFile = ImagePicker.pickImage(source: source);
+    });
+  }
+
   loadModel() async {
     String res;
     switch (_model) {
+      case camera:
+        res = await Tflite.loadModel(
+          model: "assets/yolov2_tiny.tflite",
+          labels: "assets/yolov2_tiny.txt",
+        );
+        break;
       case yolo:
         res = await Tflite.loadModel(
           model: "assets/yolov2_tiny.tflite",
@@ -80,21 +96,14 @@ class _HomePageState extends State<HomePage> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
+                  showImage(),
+                  RaisedButton(
+                    child: const Text(camera),
+                    onPressed: () => pickImageFromGallery(ImageSource.gallery),
+                  ),
                   RaisedButton(
                     child: const Text(ssd),
                     onPressed: () => onSelect(ssd),
-                  ),
-                  RaisedButton(
-                    child: const Text(yolo),
-                    onPressed: () => onSelect(yolo),
-                  ),
-                  RaisedButton(
-                    child: const Text(mobilenet),
-                    onPressed: () => onSelect(mobilenet),
-                  ),
-                  RaisedButton(
-                    child: const Text(posenet),
-                    onPressed: () => onSelect(posenet),
                   ),
                 ],
               ),
@@ -115,6 +124,32 @@ class _HomePageState extends State<HomePage> {
                     _model),
               ],
             ),
+    );
+  }
+
+  Widget showImage() {
+    return FutureBuilder<File>(
+      future: imageFile,
+      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.data != null) {
+          return Image.file(
+            snapshot.data,
+            width: 300,
+            height: 300,
+          );
+        } else if (snapshot.error != null) {
+          return const Text(
+            'Error Picking Image',
+            textAlign: TextAlign.center,
+          );
+        } else {
+          return const Text(
+            'No Image Selected',
+            textAlign: TextAlign.center,
+          );
+        }
+      },
     );
   }
 }
--- lib/models.dart
+++ lib/models.dart
@@ -2,3 +2,5 @@ const String mobilenet = "MobileNet";
 const String ssd = "SSD MobileNet";
 const String yolo = "Tiny YOLOv2";
 const String posenet = "PoseNet";
+const String camera = "Camera";
+const String show = "Show";
